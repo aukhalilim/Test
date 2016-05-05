73d92db8f54b0c51029dc3c833fd29ed677f655e
imran_khalil@hotmail.com <imran_khalil@hotmail.com>
2016-05-05 11:17:50 +1000

    Testing for GIT

---
+++ AutoFac.Sample/Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoFac.Sample.Models;
+
+namespace AutoFac.Sample.Controllers
+{
+    public class BlogController : ApiController
+    {
+
+        private IPostRepository _repository;
+        
+        public BlogController(IPostRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        public IEnumerable<Post> Recent(int retrievalCount)
+       {
+           return _repository.ListRecentPosts(retrievalCount);
+       }
+    
+       [HttpPost]
+       public void Add(Post post)
+       {
+           _repository.Add(post);
+       }    
+    }
+}
---
+++ AutoFac.Sample/Models/IPostRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFac.Sample.Models
+{
+    public interface IPostRepository
+    {
+        void Add(Post post);
+        IList<Post> ListRecentPosts(int retrievalCount);
+    }
+
+   public class Post
+   {
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public string Url { get; set; }
+    }
+}
